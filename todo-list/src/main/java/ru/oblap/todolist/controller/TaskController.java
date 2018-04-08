a80d236bc1b40893bba046cf3988417b6855bfbd
Maxim Obrubov <obrubov.max@mail.ru>
2018-04-08 19:24:27 +0300

    Написан API TaskController

--- todo-list/src/main/java/ru/oblap/todolist/controller/TaskController.java
+++ todo-list/src/main/java/ru/oblap/todolist/controller/TaskController.java
@@ -1,7 +1,32 @@
 package ru.oblap.todolist.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.oblap.todolist.model.Task;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class TaskController {
+
+    @RequestMapping(value = "/add", method = RequestMethod.PUT)
+    public Task add(@RequestParam("task") Task task) {
+
+        return null;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Task> getTasks() {
+
+        return null;
+    }
+
+    @RequestMapping(value = "/done", method = RequestMethod.PUT)
+    public boolean isDone() {
+
+        return false;
+    }
 }
